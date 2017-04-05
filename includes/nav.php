c3c1711d7310532c7a0231d098d2f51511d23b03
CuongLai <Laic5000@gmail.com>
2017-04-05 12:23:57 -0400

    Debugged createBracket.php and nav.php

--- includes/nav.php
+++ includes/nav.php
@@ -5,13 +5,13 @@
       if ($PATH_PARTS['filename'] == 'index') {
         print ' class="activePage" ';
       }
-      print '><a href="index.php">Home</a></li>';
+      print '><a href="./index.php">Home</a></li>';
 
-      print '<li';
+      print '<li ';
       if ($PATH_PARTS['filename'] == 'createBracket') {
         print ' class="activePage" ';
       }
-      print '><a href="form-trails.php?trailId=0">Start a bracket</a></li>';
+      print '><a href="createBracket.php">Start a bracket</a></li>';
     ?>
   </ol>
 </nav>

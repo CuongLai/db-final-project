c17f46265ca7a086e472ef9e2e435d0b350795f9
Nel Korajkic <nelkorajkic@gmail.com>
2017-04-05 23:28:09 -0400

    finished styling create bracket for now

--- createBracket.php
+++ createBracket.php
@@ -4,15 +4,24 @@
 </div>
 <article>
 <form action="#" method="post">
-<h2>Enter your bracket name:</h2>
-<input type="text" name="bracketName" value="" />
-<h2>Type of tournament bracket:</h2>
-<input type="radio" name="elimination" value="0"> Single
-<input type="radio" name="elimination" value="1"> Double
-<h2>Enter number of entrants:</h2>
-<input type="number" name="bracketSize" value="bracketSize" />
 
-<input class="button" id="btnSubmit" name="btnSubmit" tabindex="900" type="submit" value="Create Bracket" >
+<div class="formGroup centerText">
+  <h2 class="customH2">Enter your bracket name:</h2>
+  <input type="text" name="bracketName" value="" />
+</div>
+
+<div class="formGroup centerText">
+  <h2 class="customH2">Type of tournament bracket:</h2>
+  <input class="" type="radio" name="elimination" value="0"> Single
+  <input class="" type="radio" name="elimination" value="1"> Double
+</div>
+
+<div class="formGroup centerText">
+  <h2 class="customH2">Enter number of entrants:</h2>
+  <input type="number" name="bracketSize" value="bracketSize" />
+</div>
+
+<input class="mainBtn formGroup centerText" id="btnSubmit" name="btnSubmit" tabindex="900" type="submit" value="Create Bracket" >
 </form>
 
 </article>

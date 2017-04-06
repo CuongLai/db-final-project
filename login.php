d947068e845d5589117baa65d7d9252b924bf0ea
CuongLai <Laic5000@gmail.com>
2017-04-06 00:03:32 -0400

    Login and sessions works

--- login.php
+++ login.php
@@ -4,27 +4,41 @@
   <h1>Log in to create and access your brackets!</h1>
 
   <label>Username: </label>
-  <input type="text" class="login" placeholder="Please enter your username..." name="username">
+  <input type="text" class="login" placeholder="Please enter your username..." value="cuong" name="username">
 
   <label>Password: </label>
-  <input type="password" class="login" placeholder="Please enter your password..." name="password">
+  <input type="password" class="login" placeholder="Please enter your password..." value="admin" name="password">
 
   <input type="submit" class="loginBtn" value="Log In" name="login">
 </form>
 
 <?php
   if (isset($_POST['login'])) {
-    $credentials = array();
-    $username = htmlentities($_POST["username"], ENT_QUOTES, "UTF-8");
-    $credentials[] = $username;
-    $password = htmlentities($_POST["password"], ENT_QUOTES, "UTF-8");
-    $credentials[] = $password;
+    $_SESSION['user'] = isset($_SESSION['user']) ? $_SESSION['user'] : '';
 
-    foreach ($credentials as $credential) {
-      echo $credential;
+    $data = array();
+    $username = htmlentities($_POST["username"], ENT_QUOTES, "UTF-8");
+    $data[] = $username;
+    $password = htmlentities($_POST["password"], ENT_QUOTES, "UTF-8");
+
+    $errors = array();
+
+    if(!$_SESSION['user']) {
+      $query = 'SELECT * FROM tblUsers WHERE fldUsername=?';
+      $results = $thisDatabaseReader->select($query, $data);
+
+      if (!empty($results)) {
+        if ($results[0]['fldPassword'] == $password) {
+          session_write_close();
+          header('Location:index.php');
+        }
+        else {
+          echo 'Wrong username or password';
+        }
+      }
+      else {
+        echo 'Wrong username or password';
+      }
     }
   }
-	// else:
-	// 	header("Location:hub.php");
-	// endif;
 ?>

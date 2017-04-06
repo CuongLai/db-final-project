3a84e198837a80b2cfe073a66c6acfd724be530c
Nel Korajkic <nelkorajkic@gmail.com>
2017-04-05 23:10:28 -0400

    more styling set up the form in create bracket

--- createBracket.php
+++ createBracket.php
@@ -1,9 +1,20 @@
-<?php include "includes/top.php"; ?>
-
-<h1 class="centerText customH1"> Please Create your bracket</h1>
+<?php require "includes/top.php"; ?>
+<div class="welcomeContainer">
+  <h1 class="centerText customH1"> Please create your bracket</h1>
+</div>
 <article>
+<form action="#" method="post">
+<h2>Enter your bracket name:</h2>
+<input type="text" name="bracketName" value="" />
+<h2>Type of tournament bracket:</h2>
+<input type="radio" name="elimination" value="0"> Single
+<input type="radio" name="elimination" value="1"> Double
+<h2>Enter number of entrants:</h2>
+<input type="number" name="bracketSize" value="bracketSize" />
 
+<input class="button" id="btnSubmit" name="btnSubmit" tabindex="900" type="submit" value="Create Bracket" >
+</form>
 
 </article>
 
-<?php include "includes/footer.php"; ?>
+<?php require "includes/footer.php"; ?>

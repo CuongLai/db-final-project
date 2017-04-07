3f00bf0cb3aef155dde5db12c51bac905bfabc94
CuongLai <Laic5000@gmail.com>
2017-04-07 14:04:03 -0400

    Added code in top so if a user doesn't perform an action in 1 hour, the session is destroyed

--- includes/top.php
+++ includes/top.php
@@ -32,4 +32,9 @@
     if ($location != 'login' && $location != 'logout' && $location != 'signup') {
       require 'nav.php';
     }
+    if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 3600)) {
+      session_unset();
+      session_destroy();
+    }
+    $_SESSION['LAST_ACTIVITY'] = time();
   ?>

0a9579103ce6e271c16bd07323310607f10e2707
Nel Korajkic <nelkorajkic@gmail.com>
2017-04-07 13:45:01 -0400

    added an example image of the bracket into the index page

--- index.php
+++ index.php
@@ -7,11 +7,15 @@
 
 //Added this stuff. see if it appears for you
   if (!isset($_SESSION['user'])) {
+    print '<div class="left-40">';
     print '<h1 class="floatLeft customH1 subH1">Ready to get your tournament going?</h1>';
     print '<p class="floatleft customP subHeading">Use our simple bracket manager to make your tournament! For any of your favorite games.</p>';
+    print'</div>';
+    print '<div class="right-40">';
+    print '<img class="floatRight bracketExampleImage" src="./css/images/bracket-example.png" />';
+    print '</div>';
     print '</div>';
     print '<button class="mainBtn"><a class="linkBtn" href="signUp.php">Sign up</a></button>';
-    print '</div>';
   }
   else {
     print '<h1 class="floatLeft customH1 subH1">Start a bracket for your tournament!</h1>';

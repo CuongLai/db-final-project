dfc1d3538bf9a9261f45a9b09b75e51e0c46b729
Nel Korajkic <nelkorajkic@gmail.com>
2017-04-06 23:39:33 -0400

    index styling

--- index.php
+++ index.php
@@ -3,19 +3,14 @@
   print '<div class="welcomeContainer">';
   print '<h1 class="welcomeH1 centerText textShadow"> Welcome to</h1>';
   print '<img class="centerImg" src="./css/images/logo.png" alt="logo for our website" />';
+  print '<div class="clear">';
+  print '<h1 class="floatLeft customH1 subH1">Ready to get your tournament going?</h1>';
+  print '<p class="floatleft customH2 subHeading">Use our simple bracket manager to make your tournament! For any of your favorite games.</p>';
   print '</div>';
-  if (!isset($_SESSION['user'])) {
+  print '<button class="mainBtn"><a class="linkBtn" href="signUp.php">Sign up</a></button>';
+  print '</div>';
 ?>
-<h1>Ready to get your tournament going?</h1>
-<p>Use our simple bracket manager to make your tournament! For any of <b>your favorite games</b></p>
-<button class="mainBtn"><a href="signup.php">Sign up</a></button>
+
 <?php
-  }
-  else {
-?>
-<h1>Create a new bracket</h1>
-<button class="mainBtn"><a href="createBracket.php">Create</a></button>
-<?php
-  }
   require 'includes/footer.php';
 ?>

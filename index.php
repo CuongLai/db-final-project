1360d5a223737a79976c83aae74e96e856974eff
Nel Korajkic <nelkorajkic@gmail.com>
2017-04-07 20:48:38 -0400

    more styling on index LOOKING GOOD

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
   require 'includes/top.php';
   print '<div class="welcomeContainer">';
+  print '<div class="middle-80">';
   print '<h1 class="welcomeH1 centerText textShadow"> Welcome to</h1>';
   print '<img class="centerImg" src="./css/images/logo.png" alt="logo for our website" />';
   print '<div class="clear">';
@@ -11,11 +12,12 @@
     print '<h1 class="floatLeft customH1 subH1">Ready to get your tournament going?</h1>';
     print '<p class="floatleft customP subHeading">Use our simple bracket manager to make your tournament! For any of your favorite games.</p>';
     print'</div>';
-    print '<div class="right-40">';
+    print '<div class="right-50">';
     print '<img class="floatRight bracketExampleImage" src="./css/images/bracket-example.png" />';
     print '</div>';
     print '</div>';
     print '<button class="mainBtn"><a class="linkBtn" href="signUp.php">Sign up</a></button>';
+    print '</div>';
   }
   else {
     print '<h1 class="floatLeft customH1 subH1">Start a bracket for your tournament!</h1>';

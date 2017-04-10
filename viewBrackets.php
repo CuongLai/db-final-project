99c5c021a4c166ac04fb9e7192f4b617250cea15
Nel Korajkic <nelkorajkic@gmail.com>
2017-04-09 23:35:36 -0400

    styled the viewBrackets page, the table items are still janky

--- viewBrackets.php
+++ viewBrackets.php
@@ -4,18 +4,23 @@
     $query = 'SELECT * FROM tblBrackets';
     $results = $thisDatabaseReader->select($query, '');
 ?>
-<table>
-  <tr>
-    <th>Bracket Name</th>
-    <th>Elimination Type</th>
-    <th># of Players</th>
-    <th>Status</th>
-  </tr>
+<div class="middle-80">
+
+<div class="viewBracketsTitleContainer">
+  <h1 class = "centerText customH2">Your Brakets</h1>
+</div>
 <?php
   if (!empty($results)) {
     foreach ($results as $result) {
+      print'<table class="viewBracketsTableContainer">';
+        print'<tr class ="tableRow">';
+          print'<th>Bracket Name</th>';
+          print'<th>Elimination Type</th>';
+          print'<th># of Players</th>';
+          print'<th>Status</th>';
+        print'</tr>';
       print '<tr href="bracket.php?id=' . $result['pmkBracketId'] . '">';
-      print '<td><a href="bracket.php?id=' . $result['pmkBracketId'] . '">';
+      print '<td class="tableRow"><a href="bracket.php?id=' . $result['pmkBracketId'] . '">';
       print $result['fldBracketName'] . '</a></td>';
       if ($result['fldElim'] == 0) {
         print '<td>Single</td>';
@@ -34,6 +39,7 @@
     }
   }
   print '</table>';
+  print '</div>';
   }
   else {
     header('Location:login.php');

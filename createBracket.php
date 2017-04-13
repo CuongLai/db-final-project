20f30cde650d6a08db5d3cb2c1eb93dbee0a294c
Nel Korajkic <nelkorajkic@gmail.com>
2017-04-12 22:40:40 -0400

    styled create bracket

--- createBracket.php
+++ createBracket.php
@@ -2,30 +2,33 @@
 require "includes/top.php";
 if (isset($_SESSION['user'])) {
 ?>
-<div class="welcomeContainer">
-  <h1 class="centerText customH1">Please create your bracket</h1>
-</div>
+<div class="middle-80 padding-10">
+<div class="createBracketContainer">
+  <h1 class="centerText loginH1">Please create your bracket</h1>
 <article>
 <form action="#" method="post">
 
 <div class="formGroup centerText">
-  <h2 class="customH2">Enter your bracket name:</h2>
+  <h2 class="loginH1">Enter your bracket name:</h2>
   <input type="text" name="bracketName" value="" />
 </div>
 
 <div class="formGroup centerText">
-  <h2 class="customH2">Type of tournament bracket:</h2>
+  <h2 class="loginH1">Type of tournament bracket:</h2>
   <input class="" type="radio" name="elimination" value="0"> Single
   <input class="" type="radio" name="elimination" value="1"> Double
 </div>
 
 <div class="formGroup centerText">
-  <h2 class="customH2">Enter number of entrants:</h2>
-  <input type="number" name="bracketSize" value="bracketSize" />
+  <h2 class="loginH1">Enter number of entrants:</h2>
+  <input type="number" name="bracketSize" value="2" />
 </div>
-
-<input class="mainBtn formGroup centerText" id="btnSubmit" name="btnSubmit" tabindex="900" type="submit" value="Create Bracket" >
+<div class="loginCenterButtons">
+<input class="mainBtn linkBtn" id="btnSubmit" name="btnSubmit" tabindex="900" type="submit" value="Create Bracket" />
+</div>
 </form>
+</div>
+</div>
 
 </article>
 

fe74092f5e0da79fe44d1e05ff5c76a14fc586f8
Nel Korajkic <nelkorajkic@gmail.com>
2017-04-20 00:45:17 -0400

    so I set up alot of SQL in createBracket.

--- createBracket.php
+++ createBracket.php
@@ -7,6 +7,79 @@ if (isset($_SESSION['user'])) {
   <h1 class="centerText customH2">Please create your bracket</h1>
 <article>
 </div>
+
+<?php
+
+//Init all the variables
+$formBracketName = "";
+$formFldElim = "";
+$formFldNumMatches = 2;
+$formFldCompletion = ""; // @REVIEW What is this?
+
+//init error messages
+$formBracketNameERROR = false;
+$formFldElimERROR = false;
+$formFldNumMatchesERROR = false;
+
+//init the data array, and the error array
+$data = array();
+$errorMsg = array();
+
+//form processing
+if (isset($_POST["btnSubmit"])) {
+
+//security check, does this actually work? @REVIEW
+if (!securityCheck($yourURL)) {
+        $msg = "<p>Sorry you cannot access this page. ";
+        $msg.= "Security breach detected and reported</p>";
+        die($msg);
+    }
+
+//SANITIZING: removing HTML and JS from inputs
+formBracketName = htmlentities($_POST["bracketName"], ENT_QUOTES, "UTF-8");
+$data[] = $formBracketName;
+
+$formFldElim = htmlentities($_POST["elimination"], ENT_QUOTES, "UTF-8");
+$data[] = $formFldElim;
+
+$formFldNumMatches = htmlentities($_POST["bracketSize"], ENT_QUOTES, "UTF-8");
+$data[] = $formFldNumMatches;
+
+//validation, error messages
+if ($formBracketName == "") {
+        $errorMsg[] = "Enter your bracket name!";
+        $formBracketNameERROR = true;
+    } elseif (!verifyAlphaNum($formBracketName)) {
+        $errorMsg[] = "Your bracket name appears to have extra characters.";
+        $formBracketNameERROR = true;
+}
+
+if ($formFldElim == "") {
+        $errorMsg[] = "Enter the type of bracket!";
+        $formFldElimERROR = true;
+    } elseif (!verifyAlphaNum($formFldElim)) {
+        $errorMsg[] = "You didn't select a type of bracket!";
+        $formFldElimERROR = true;
+}
+
+if ($formFldNumMatches < 2) {
+        $errorMsg[] = "You need atleast 2 players!";
+        $formFldNumMatches = true;
+    }
+
+//set up the query
+$query = 'INSERT INTO tblBrackets SET pmkBracketId = ?, fldBracketName = ?, fldElim = ?, fldNumMatches = ? ';
+$data = array($formBracketName, $formFldElim, $formFldNumMatches, $formFldCompletion);
+}
+
+//checking the Security of our query, insert if it's good
+if ($thisDatabaseWriter->querySecurityOk($query, 0)) {
+                $query = $thisDatabaseWriter->sanitizeQuery($query);
+                $results = $thisDatabaseWriter->insert($query, $data);
+                $primaryKey = $thisDatabaseWriter->lastInsert();
+            }
+ ?>
+
 <div class="viewBracketsTableContainer">
 <form action="#" method="post">
 <div class="formGroup centerText">

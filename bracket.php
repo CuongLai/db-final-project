18f887c08685fc586220a9def73ffb8270c1db9a
Nel Korajkic <nelkorajkic@gmail.com>
2017-04-20 23:25:53 -0400

    got the highlight on winner to work but only on first match

--- bracket.php
+++ bracket.php
@@ -9,7 +9,6 @@
   foreach ($bracket as $title) {
     print '<h1 class="welcomeH1 centerText textShadow">' . $title["fldBracketName"] . '</h1>';
   }
-
   print '<div class="bracket">';
   for ($i = 1; $i <= $bracket[0]['fldNumRounds']; $i++) {
     $data = array();
@@ -28,9 +27,20 @@
       $query = 'SELECT * FROM tblPeople WHERE pmkPlayerId=?';
       $player2 = $thisDatabaseReader->select($query, $data);
       print '<div class="match">';
-      print ' <p class="playerContainer">' . $player1[0]['fldName'] . '</p>';
+      print ' <p id= "player1Container" class="playerContainer">' . $player1[0]['fldName'] . '</p>';
       print ' <p class="scoreContainer">' . $match['fldP1Score'] . '</p>';
-      print ' <p class="playerContainer">' . $player2[0]['fldName'] . '</p>';
+      if ($match['fldP1Score'] == 3) {
+        print '<script>';
+        print "document.getElementById('player1Container').style.border='thick solid 0dd3ac';";
+        print '</script>';
+
+      }
+      print ' <p id= "player2Container" class="playerContainer">' . $player2[0]['fldName'] . '</p>';
+      if ($match['fldP2Score'] == 3) {
+        print '<script>';
+        print "document.getElementById('player2Container').style.border='thick solid #0dd3ac';";
+        print '</script>';
+      }
       print ' <p class="scoreContainer">' . $match['fldP2Score'] . '</p>';
       if ($player1[0]['pmkPlayerId'] != 0 && $player2[0]['pmkPlayerId'] != 0) {
         print ' <a href="match.php?matchId=' . $match['pmkMatchId'] . '&final=' . $bracket[0]['fldNumRounds'] . '&bracketId=' . $id . '">';

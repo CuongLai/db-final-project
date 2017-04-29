3bc6b3a5075542b674e4a9a0d3de58af9f8ee166
CuongLai <Laic5000@gmail.com>
2017-04-29 17:33:17 -0400

    Added error checking in createBracket.php

--- createBracket.php
+++ createBracket.php
@@ -4,13 +4,12 @@ if (isset($_SESSION['user'])) {
 
 //Init all the variables
 $formBracketName = "";
-$formFldNumPlayers = 2;
+$formFldNumPlayers = 4;
 $formFldCompletion = "";
 $formPlayerNames = "";
 
 //init error messages
-$formBracketNameERROR = false;
-$formFldNumMatchesERROR = false;
+$playerError = false;
 
 //init the data array, and the error array
 $data = array();
@@ -23,104 +22,94 @@ if (isset($_POST["btnSubmit"])) {
   $formFldNumPlayers = htmlentities($_POST["bracketSize"], ENT_QUOTES, "UTF-8");
   $formFldNumRounds = log($formFldNumPlayers, 2);
   $formFldCompletion = 0;
-  //validation, error messages
-  // if ($formBracketName == "") {
-  //   $errorMsg[] = "Enter your bracket name!";
-  //   $formBracketNameERROR = true;
-  // } elseif (!verifyAlphaNum($formBracketName)) {
-  //   $errorMsg[] = "Your bracket name appears to have extra characters.";
-  //   $formBracketNameERROR = true;
-  // }
-  //
-  // if ($formFldElim == "") {
-  //   $errorMsg[] = "Enter the type of bracket!";
-  //   $formFldElimERROR = true;
-  // } elseif (!verifyAlphaNum($formFldElim)) {
-  //   $errorMsg[] = "You didn't select a type of bracket!";
-  //   $formFldElimERROR = true;
-  // }
-  //
-  // if ($formFldNumMatches < 2) {
-  //   $errorMsg[] = "You need atleast 2 players!";
-  //   $formFldNumMatches = true;
-  // }
-  //
-
-  //set up the query
-  $query = 'INSERT INTO tblBrackets SET fnkUserId=?, fldBracketName=?, fldNumPlayers=?, fldNumRounds=?, fldCompletion=?';
-  $id = $_SESSION['userId'];
-  $data = array($id, $formBracketName, $formFldNumPlayers, $formFldNumRounds, $formFldCompletion);
-
-  $query = $thisDatabaseWriter->sanitizeQuery($query);
-  $results = $thisDatabaseWriter->insert($query, $data);
-  $primaryKey = $thisDatabaseWriter->lastInsert();
 
   $players = $_POST['player'];
-  $playerArray = array();
-  $query = 'SELECT * FROM tblPeople';
-  $results = $thisDatabaseReader->select($query, '');
+
+  //validation, error messages
+  if (empty($formBracketName)) {
+    $errorMsg[] = "Enter your bracket name!";
+  }
   foreach ($players as $player) {
-    $matchFound = false;
-    foreach ($results as $result) {
-      if ($player == $result['fldName']) {
-        $playerArray[] = $result['pmkPlayerId'];
-        $matchFound = true;
-      }
-    }
-    if ($matchFound == false) {
-      $data = array();
-      $data[] = $player;
-      $query = 'INSERT INTO tblPeople SET fldName=?';
-      $addedPlayer = $thisDatabaseWriter->insert($query, $data);
-      $playerArray[] = $thisDatabaseWriter->lastInsert();
+    if (empty($player)) {
+      $playerError = true;
     }
   }
-
-  $numMatches = $formFldNumPlayers;
-  $playerIndex = 0; //number that will reference the playerArray
-  for ($i=1; $i<=$formFldNumRounds; $i++) {
-    $nextMatchCount = 1;
-    $numMatches = $numMatches / 2;
-    for ($j=1; $j<=$numMatches; $j++) {
-      $data = array();
-      $data[] = $primaryKey; //fnkBracketId
-      $data[] = $i; //fldRoundId
-      $data[] = $j; //fldRoundMatchId
-      if ($j % 2 != 0) { //if odd
-        $data[] = $nextMatchCount; //fldNextMatch
-        $data[] = 1; //fldWhichPlayer
-      }
-      else { //if even
-        $data[] = $nextMatchCount; //fldNextMatch
-        $nextMatchCount++;
-        $data[] = 2; //fldWhichPlayer
-      }
-      if ($i == 1) {
-        $data[] = $playerArray[$playerIndex]; //fnkPlayer1Id
-        $playerIndex++;
-        $data[] = $playerArray[$playerIndex]; //fnkPlayer2Id
-        $playerIndex++;
-      }
-      else {
-        $data[] = 0; //fnkPlayer1Id
-        $data[] = 0; //fnkPlayer2Id
-      }
-      $data[] = 0; //fldP1Score
-      $data[] = 0; //fldP2Score
-      $query = 'INSERT INTO tblBracketsPeople SET fnkBracketId=?, fldRoundId=?, fldRoundMatchId=?, fldNextMatch=?, fldWhichPlayer=?, fnkPlayer1Id=?, fnkPlayer2Id=?, fldP1Score=?, fldP2Score=?';
-      $results = $thisDatabaseWriter->insert($query, $data);
-    }
+  if ($playerError == true) {
+    $errorMsg[] = "Enter the player names!";
   }
 
-  if ($errorMsg) {
-    print '<div id="errors">';
-    print '<h1>Your form has the following mistakes</h1>';
-    print "<ol>\n";
+  //set up the query
+  if (empty($errorMsg)) {
+    $query = 'INSERT INTO tblBrackets SET fnkUserId=?, fldBracketName=?, fldNumPlayers=?, fldNumRounds=?, fldCompletion=?';
+    $id = $_SESSION['userId'];
+    $data = array($id, $formBracketName, $formFldNumPlayers, $formFldNumRounds, $formFldCompletion);
+
+    $query = $thisDatabaseWriter->sanitizeQuery($query);
+    $results = $thisDatabaseWriter->insert($query, $data);
+    $primaryKey = $thisDatabaseWriter->lastInsert();
+
+    $playerArray = array();
+    $query = 'SELECT * FROM tblPeople';
+    $results = $thisDatabaseReader->select($query, '');
+    foreach ($players as $player) {
+      $matchFound = false;
+      foreach ($results as $result) {
+        if ($player == $result['fldName']) {
+          $playerArray[] = $result['pmkPlayerId'];
+          $matchFound = true;
+        }
+      }
+      if ($matchFound == false) {
+        $data = array();
+        $data[] = $player;
+        $query = 'INSERT INTO tblPeople SET fldName=?';
+        $addedPlayer = $thisDatabaseWriter->insert($query, $data);
+        $playerArray[] = $thisDatabaseWriter->lastInsert();
+      }
+    }
+
+    $numMatches = $formFldNumPlayers;
+    $playerIndex = 0; //number that will reference the playerArray
+    for ($i=1; $i<=$formFldNumRounds; $i++) {
+      $nextMatchCount = 1;
+      $numMatches = $numMatches / 2;
+      for ($j=1; $j<=$numMatches; $j++) {
+        $data = array();
+        $data[] = $primaryKey; //fnkBracketId
+        $data[] = $i; //fldRoundId
+        $data[] = $j; //fldRoundMatchId
+        if ($j % 2 != 0) { //if odd
+          $data[] = $nextMatchCount; //fldNextMatch
+          $data[] = 1; //fldWhichPlayer
+        }
+        else { //if even
+          $data[] = $nextMatchCount; //fldNextMatch
+          $nextMatchCount++;
+          $data[] = 2; //fldWhichPlayer
+        }
+        if ($i == 1) {
+          $data[] = $playerArray[$playerIndex]; //fnkPlayer1Id
+          $playerIndex++;
+          $data[] = $playerArray[$playerIndex]; //fnkPlayer2Id
+          $playerIndex++;
+        }
+        else {
+          $data[] = 0; //fnkPlayer1Id
+          $data[] = 0; //fnkPlayer2Id
+        }
+        $data[] = 0; //fldP1Score
+        $data[] = 0; //fldP2Score
+        $query = 'INSERT INTO tblBracketsPeople SET fnkBracketId=?, fldRoundId=?, fldRoundMatchId=?, fldNextMatch=?, fldWhichPlayer=?, fnkPlayer1Id=?, fnkPlayer2Id=?, fldP1Score=?, fldP2Score=?';
+        $results = $thisDatabaseWriter->insert($query, $data);
+      }
+    }
+    header("Location:viewBrackets.php");
+  }
+
+  else {
     foreach ($errorMsg as $err) {
-      print "<li>" . $err . "</li>\n";
+      print '<h1 class="centerText customH2">' . $err . '</h1>';
     }
-    print "</ol>\n";
-    print '</div>';
   }
 }
 ?>
@@ -140,7 +129,6 @@ if (isset($_POST["btnSubmit"])) {
 <div class="formGroup centerText">
   <h2 class="loginH1">Enter number of entrants:</h2>
   <select class="numNames" name="bracketSize">
-    <option></option>
     <option value="4">4</option>
     <option value="8">8</option>
     <option value="16">16</option>
@@ -149,7 +137,20 @@ if (isset($_POST["btnSubmit"])) {
 <div class="loginCenterButtons">
 
 <h2 class="loginH1">Enter entrant names:</h2>
-<div id="nameText" class="formGroup centerText"></div>
+<div id="nameText" class="formGroup centerText">
+  <div>
+    <input type="text" class="name" name="player[]" placeholder="Add new player">
+  </div>
+  <div>
+    <input type="text" class="name" name="player[]" placeholder="Add new player">
+  </div>
+  <div>
+    <input type="text" class="name" name="player[]" placeholder="Add new player">
+  </div>
+  <div>
+    <input type="text" class="name" name="player[]" placeholder="Add new player">
+  </div>
+</div>
 
 <input class="mainBtn linkBtn" id="btnSubmit" name="btnSubmit" tabindex="900" type="submit" value="Create Bracket" />
 </div>
